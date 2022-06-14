bc8b56e15137f29f510be5dd6f5ac3eaec1da1f8
Austin <austinpaul606@gmail.com>
2022-06-14 20:13:23 +0100

    Added the Airplay page

--- src/components/About/Airplay.js
+++ src/components/About/Airplay.js
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
 import "./about.css";
 const Airplay = () => {
   return (
     <div className="airplay__container">
       <div className="airplay__items">
         <h3>AirPlay</h3>
-        <h1>A higher definition of TV.</h1>
+        <h1>Bring Apple TV+ to a screen near you.</h1>
+        <Link to="/">Learn more</Link>
         <div className="airplay__image">
           <img src="./images/AppleAirplayLogo.png" alt="" />
         </div>
